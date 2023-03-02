4800569fa0ec08e0f4b9296107ab9a854e55804f
nikoloz baratashvili <n.baratashvili@itstep.ge>
2023-03-02 23:18:27 +0400

    Added navbar responsive and started making resposive settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ResponnsiveNavbarOverlayComponent } from './responnsive-navbar-overlay/responnsive-navbar-overlay.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    ResponnsiveNavbarOverlayComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/responnsive-navbar-overlay/responnsive-navbar-overlay.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-responnsive-navbar-overlay',
+  templateUrl: './responnsive-navbar-overlay.component.html',
+  styleUrls: ['./responnsive-navbar-overlay.component.css']
+})
+export class ResponnsiveNavbarOverlayComponent {
+
+}
